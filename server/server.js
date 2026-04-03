06f79f1993091d5ab5528d0f0cda08cf623da465
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-04-02 23:31:09 -0700

    feat(server): refactor database connection to use async/await

--- server/server.js
+++ server/server.js
@@ -9,15 +9,20 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
-// Connecting Database
-connectDB();
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('CodeBuddy API is running');
 }); 
 
-const Port = process.env.PORT || 8000
+const port = process.env.PORT || 8000
 
-app.listen(Port, () => {
-  console.log(`Server is running at http://localhost:${Port}`);
-});
+(async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server', error);
+    process.exit(1);
+  }
+})(); 
