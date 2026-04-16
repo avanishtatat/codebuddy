710b065980f8840539d43dac71023b2daca3a2b7
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-04-16 16:09:06 +0530

    feat(models): add Message and User schemas for MongoDB

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,},
+    email: {
+        type: String,
+        required: true,
+        unique: true,},
+    password: {
+        type: String,
+        required: true,},
+    messagesUsedToday: {
+        type: Number,
+        default: 0,
+    },
+    lastMessageDate: {
+        type: Date,
+        default: null,
+    },
+}, { timestamps: true });   
+
+module.exports = mongoose.model("User", userSchema);
