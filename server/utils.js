abad088bc8aebeda03fb26199358d6947310da27
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-04-20 09:04:33 +0530

    feat(auth): add validation for user object in token generation

--- server/utils.js
+++ server/utils.js
@@ -1,10 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (user) => {
+    if (!user || !user._id || !user.name) {
+        console.error("Invalid user object passed to generateToken", user);
+        throw new Error("Invalid user data");
+    }
+    
     if (!process.env.JWT_SECRET) {
         console.error("JWT_SECRET environment variable is not defined");
         throw new Error("Server configuration error");
-    };
+    }
 
     return jwt.sign(
         { id: user._id, name: user.name },
